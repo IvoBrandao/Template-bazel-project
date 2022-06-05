6c5a83b481dea2e4213d1f88ceb1921041cdd4ff
Ivo Brandao <ivobrd@gmail.com>
2022-06-05 19:57:25 +0200

    Initial project structure

---
+++ lib/pkg/inc/pkg_internal.hpp
@@ -0,0 +1,18 @@
+#ifndef LIB_PKG_INTERNAL_HPP_
+#define LIB_PKG_INTERNAL_HPP_
+
+namespace lib
+{
+
+    namespace pkg
+    {
+        class PkgInternal
+        {
+            public:
+                PkgInternal();
+                virtual ~PkgInternal();
+        };
+    } // namespace pkg
+} // namespace lib
+
+#endif // LIB_PKG_INTERNAL_HPP_
